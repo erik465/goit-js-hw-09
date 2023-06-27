cc596ee68d0c5c91424452b1c9472084ae0c5022
erik465 <73741157+erik465@users.noreply.github.com>
2023-06-27 12:34:07 +0000

    Deploying to gh-pages from @ erik465/goit-js-hw-09@8df5357f600609e03c67c372701adb8405ae8ecb 🚀

---
+++ 01-color-switcher.ba6b9b70.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(()=>{t.disabled=!0,a=setInterval((()=>{document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.ba6b9b70.js.map
